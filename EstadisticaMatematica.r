39e88aca9e62b06eecbeeca8ca09eff0b70266a7
Francisco ME <if.francisco.me@gmail.com>
2015-11-09 11:45:03 -0600

    Inicial

---
+++ EstadisticaMatematica.r
@@ -0,0 +1,16 @@
+
+% -- ----------------------------------------------------------------------------------------- -- #
+% -- ITESO, Universidad Jesuita de Guadalajara ----------------------------------------------- -- #
+% -- Ingeniería Financiera - Departamento de Matematicas y Fisica ---------------------------- -- #
+% -- Licencia: GNU General Public License ---------------------------------------------------- -- #
+% -- ----------------------------------------------------------------------------------------- -- #
+
+% -- Funcion de Auto Correlacion y e Auto Correlacion Parcial -------------------------------- -- #
+
+AutoCorrelation <- function(x, type, LagMax)  {
+  ciline <- 2/sqrt(length(x))
+  bacf   <- acf(x, plot = FALSE, lag.max = LagMax, type = type)
+  bacfdf <- with(bacf, data.frame(lag, acf))
+  Sig_nc <- (abs(bacfdf[,2]) > abs(ciline))^2
+  bacfdf <- cbind(bacfdf, Sig_nc)
+return(bacfdf) }
